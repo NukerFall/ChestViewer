d2f6a6a4996b91f904b6b1b3f958376b4c8511d1
NukerFall <nukerfall@gmail.com>
2019-12-02 17:54:16 +0300

    PressF

--- src/main/java/me/NukerFall/ChestViewer/Events/Click.java
+++ src/main/java/me/NukerFall/ChestViewer/Events/Click.java
@@ -26,47 +26,51 @@ public class Click implements Listener {
 		Chest c = null;
 		if (e.getAction() == Action.RIGHT_CLICK_BLOCK && e.getClickedBlock().getType() == Material.CHEST) {
 			c = (Chest) e.getClickedBlock().getState();
-		}
-			if (e.getAction() == Action.RIGHT_CLICK_AIR && e.getPlayer().getTargetBlock(null, 100).getType() == Material.CHEST) {
+		} else if (e.getAction() == Action.RIGHT_CLICK_AIR && e.getPlayer().getTargetBlock(null, 100).getType() == Material.CHEST) {
 			c = (Chest) e.getPlayer().getTargetBlock(null, 100).getState();
+		} else {
+			return;
 		}
-				if (e.getPlayer().hasPermission("chestviewer.view")) {
-					if (main.isWork(e.getPlayer())) {
-						if ((main.getConfig().getBoolean("item-event")
-							&& e.getPlayer().getInventory().getItemInMainHand().getType() == Material.valueOf(main.getConfig().getString("item-material")))
-							|| !main.getConfig().getBoolean("item-event")) {
-							Player p = e.getPlayer();
-							if (!main.getConfig().getBoolean("gui")) {
-								for (ItemStack i : c.getInventory().getContents()) {
-									if (i!=null && !main.getConfig().getStringList("blacklisted-items").contains(i.getType().name())) {
-										if (main.getConfig().getBoolean("show-amount")) {
-											p.sendMessage(Colors.clr(main.getConfig().getString("item-type-amount")
-											.replaceAll("%item%", i.getType().name().toLowerCase()).replaceAll("%num%", String.valueOf(i.getAmount()))));
-										} else {
-											p.sendMessage(Colors.clr(main.getConfig().getString("item-type")
-											.replaceAll("%item%", i.getType().name().toLowerCase())));
-										}
-									}
+		if (e.getPlayer().hasPermission("chestviewer.view")) {
+			if (main.isWork(e.getPlayer())) {
+				if ((main.getConfig().getBoolean("item-event")
+				&& e.getPlayer().getInventory().getItemInMainHand().getType() == Material.valueOf(main.getConfig().getString("item-material")))
+				|| !main.getConfig().getBoolean("item-event")) {
+					Player p = e.getPlayer();
+					if (!main.getConfig().getBoolean("gui")) {
+						for (ItemStack i : c.getInventory().getContents()) {
+							if (i!=null && !main.getConfig().getStringList("blacklisted-items").contains(i.getType().name())) {
+								if (main.getConfig().getBoolean("show-amount")) {
+									p.sendMessage(Colors.clr(main.getConfig().getString("item-type-amount")
+									.replaceAll("%item%", i.getType().name().toLowerCase()).replaceAll("%num%", String.valueOf(i.getAmount()))));
+								} else {
+									p.sendMessage(Colors.clr(main.getConfig().getString("item-type")
+									.replaceAll("%item%", i.getType().name().toLowerCase())));
 								}
-							} else {
-								Inventory inv = Bukkit.createInventory(e.getPlayer(), 54, Colors.clr(main.getConfig().getString("chest-name")));
-								for (ItemStack i : c.getInventory().getContents()) {
-									if (i!=null && !main.getConfig().getStringList("blacklisted-items").contains(i.getType().name())) {
-										if (main.getConfig().getBoolean("show-amount")) {
-										inv.addItem(i);
-										} else {
-											inv.addItem(new ItemStack(i.getType(), 1));
-										}
-									}
-								}
-								p.openInventory(inv);
 							}
-						}  else {
-							e.getPlayer().sendMessage(Colors.clr(main.getConfig().getString("no-item")));
 						}
+						} else {
+						Inventory inv = Bukkit.createInventory(e.getPlayer(), 54, Colors.clr(main.getConfig().getString("chest-name")));
+						for (ItemStack i : c.getInventory().getContents()) {
+							if (i!=null && !main.getConfig().getStringList("blacklisted-items").contains(i.getType().name())) {
+								if (main.getConfig().getBoolean("show-amount")) {
+									inv.addItem(i);
+								} else {
+									inv.addItem(new ItemStack(i.getType(), 1));
+								}
+							}
+						}
+						p.openInventory(inv);
 					}
-				} else {
-					e.getPlayer().sendMessage(Colors.clr(main.getConfig().getString("no-perm")));
+					if (main.getConfig().getBoolean("remove-item") && main.getConfig().getBoolean("item-event")) {
+						p.getInventory().setItemInMainHand(null);
+					}
+				}  else {
+					e.getPlayer().sendMessage(Colors.clr(main.getConfig().getString("no-item")));
 				}
+			}
+		} else {
+			e.getPlayer().sendMessage(Colors.clr(main.getConfig().getString("no-perm")));
+		}
 	}
 }
